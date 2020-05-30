a92ce1306ac429c28d6a69235dcddcbe1ba7682d
igloo15 <mtolland15@gmail.com>
2020-05-30 00:55:15 -0400

    #Change update npm packages
    #Change update version changed to use edit json file and globbing
    #Fix webpack now correctly makes source files
    #Add vscode debugging added

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
-    devtool: 'eval',
+    devtool: 'eval-source-map',
     devServer: {
         contentBase: './dist'
     },
